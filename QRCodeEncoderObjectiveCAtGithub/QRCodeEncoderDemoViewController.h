1e72803080b104884f805e8bf6604dbe3d9dc7e1
Ming Yang <myang.git@gmail.com>
2011-07-12 20:04:23 -0400

    an example showing how to encode and render the result

---
+++ QRCodeEncoderObjectiveCAtGithub/QRCodeEncoderDemoViewController.h
@@ -0,0 +1,17 @@
+//
+//  QRCodeDemoViewController.h
+//  QRCodeEncoderObjectiveCAtGithub
+//
+//  Created by Ming Yang on 2011/7/12.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "QREncoder.h"
+#import "DataMatrix.h"
+
+@interface QRCodeEncoderDemoViewController : UIViewController {
+    
+}
+
+@end
