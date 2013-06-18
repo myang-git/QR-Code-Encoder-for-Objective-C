ece2c6c2688e1f369a2348ea3a8942713cd8f8eb
Shiva Huang <shiva.huang@cocoazen.com>
2013-06-18 22:04:49 +0800

    Add empty subclass of UIView to project.

---
+++ QRCodeEncoderObjectiveCAtGithub/QRImageView.h
@@ -0,0 +1,13 @@
+//
+//  QRImageView.h
+//  QRCodeEncoderObjectiveCAtGithub
+//
+//  Created by Shiva Huang on 13/6/18.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QRImageView : UIView
+
+@end
---
+++ QRCodeEncoderObjectiveCAtGithub/QRImageView.m
@@ -0,0 +1,31 @@
+//
+//  QRImageView.m
+//  QRCodeEncoderObjectiveCAtGithub
+//
+//  Created by Shiva Huang on 13/6/18.
+//
+//
+
+#import "QRImageView.h"
+
+@implementation QRImageView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
